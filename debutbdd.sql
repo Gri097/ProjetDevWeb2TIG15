d2632776b36b28d6ccd37aa871a3d0e927336c6c
Igor Vandervelden <34266875+Gri097@users.noreply.github.com>
2020-03-23 20:20:08 +0100

    debut bdd

---
+++ debutbdd.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM parcourbio.arbresremarquables
+LIMIT 0, 1000
+
+-- Date: 2020-03-23 20:18
+*/
+INSERT INTO `` (`ID`,`NomScientifique`,`NomFrançais`,`Famille`,`EcorcesRemarquables`,`Adefinir`,`Parcours`,`NumParcours`) VALUES (1,'Salix alba cv. tristis','','Salix','','','Parcours des Sciences',1);
+INSERT INTO `` (`ID`,`NomScientifique`,`NomFrançais`,`Famille`,`EcorcesRemarquables`,`Adefinir`,`Parcours`,`NumParcours`) VALUES (2,'Acer pensylvanicum ',' ','Acer ',' ',' ','Parcours des Sciences ',2);
+INSERT INTO `` (`ID`,`NomScientifique`,`NomFrançais`,`Famille`,`EcorcesRemarquables`,`Adefinir`,`Parcours`,`NumParcours`) VALUES (3,'Cupressus Sempervirens ',' ',' ',' ',' ','Parcours des Sciences ',3);
+INSERT INTO `` (`ID`,`NomScientifique`,`NomFrançais`,`Famille`,`EcorcesRemarquables`,`Adefinir`,`Parcours`,`NumParcours`) VALUES (4,'Carya illinonensis ',' ',' ',' ',' ','Parcours des Sciences ',4);
+INSERT INTO `` (`ID`,`NomScientifique`,`NomFrançais`,`Famille`,`EcorcesRemarquables`,`Adefinir`,`Parcours`,`NumParcours`) VALUES (5,'Taxus baccata ',' ',' ',' ',' ','Parcours des Sciences ',5);
+INSERT INTO `` (`ID`,`NomScientifique`,`NomFrançais`,`Famille`,`EcorcesRemarquables`,`Adefinir`,`Parcours`,`NumParcours`) VALUES (6,'Picea omorika ',' ',' ',' ',' ','Parcours des Sciences ',6);
+INSERT INTO `` (`ID`,`NomScientifique`,`NomFrançais`,`Famille`,`EcorcesRemarquables`,`Adefinir`,`Parcours`,`NumParcours`) VALUES (7,'Ulmus minor ',' ',' ',' ',' ','Parcours des Sciences ',7);
+INSERT INTO `` (`ID`,`NomScientifique`,`NomFrançais`,`Famille`,`EcorcesRemarquables`,`Adefinir`,`Parcours`,`NumParcours`) VALUES (8,'Acer capillipes ',' ','Acer ',' ',' ','Parcours des Sciences ',8);
+INSERT INTO `` (`ID`,`NomScientifique`,`NomFrançais`,`Famille`,`EcorcesRemarquables`,`Adefinir`,`Parcours`,`NumParcours`) VALUES (9,'Quercus robur cv. fastigiata ',' ','Quercus ',' ',' ','Parcours des Sciences ',9);
+INSERT INTO `` (`ID`,`NomScientifique`,`NomFrançais`,`Famille`,`EcorcesRemarquables`,`Adefinir`,`Parcours`,`NumParcours`) VALUES (10,'Ulmus minor cv. variegata ',' ',' ',' ',' ','Parcours des Sciences ',10);
