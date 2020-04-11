450a5b191278a86f8b38d47c0f5d73028e106220
Robin <robin@castermane.be>
2020-04-11 13:58:53 +0200

    database alter table

--- backend/src/biodiversite/models.py
+++ backend/src/biodiversite/models.py
@@ -4,25 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-
-class PointBiodiversite(models.Model):
-    idPoint = models.AutoField(primary_key=True)
-    nomFr = models.CharField(max_length=120)
-    nomSc = models.CharField(max_length=120)
-    idFamille = models.PositiveIntegerField()
-    idParcours = models.PositiveIntegerField()
-    numeroParcours = models.PositiveIntegerField()
-    ecorceRemarquable = models.BooleanField(default=False)
-
-    class Meta:
-        db_table = "pointBio"
-
-    def __str__(self):
-        return self.nomFr
-
-
 class Parcours(models.Model):
-    id = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=255)
 
     class Meta:
@@ -33,7 +15,6 @@ class Parcours(models.Model):
 
 
 class Famille(models.Model):
-    id = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=255)
 
     class Meta:
@@ -41,3 +22,19 @@ class Famille(models.Model):
 
     def __str__(self):
         return self.nom
+
+
+class PointBiodiversite(models.Model):
+    idPoint = models.AutoField(primary_key=True)
+    nomFr = models.CharField(max_length=120)
+    nomSc = models.CharField(max_length=120)
+    famille = models.ForeignKey(Famille, on_delete=models.CASCADE)
+    parcours = models.ForeignKey(Parcours, on_delete=models.CASCADE)
+    numeroParcours = models.PositiveIntegerField()
+    ecorceRemarquable = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "pointBio"
+
+    def __str__(self):
+        return self.nomFr
