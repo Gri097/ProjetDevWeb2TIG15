14288614ca32102aa5fd7973b6fff45a2ed71292
Robin <robin@castermane.be>
2020-05-10 18:12:33 +0200

    Navigation bar responsive (Etape 3)

--- FrontEnd/biodiversite/src/components/NavBar.js
+++ FrontEnd/biodiversite/src/components/NavBar.js
@@ -1,23 +1,32 @@
 import React, {Component, Fragment} from "react";
 import * as ReactBootStrap from 'react-bootstrap';
 import {Link} from "react-router-dom";
-
+//style={{backgroundColor:'#28a745'}}
 class NavBar extends Component {
     render() {
         console.log(this.props)
         return (
             <Fragment>
-                <ReactBootStrap.Navbar collapseOnSelect expand="sm" style={{backgroundColor:'#28a745', color:'white'}} >
+                <ReactBootStrap.Navbar  collapseOnSelect expand="sm" bg="dark" variant="dark">
                     <ReactBootStrap.Navbar.Brand href="/">Biodiversite LLN</ReactBootStrap.Navbar.Brand>
                     <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
                         <ReactBootStrap.Nav className="mr-auto">
-                            <ReactBootStrap.Nav.Link href="/">Acceuil</ReactBootStrap.Nav.Link>
-                            <ReactBootStrap.Nav.Link href="/parcours">Parcours</ReactBootStrap.Nav.Link>
-                            <ReactBootStrap.Nav.Link href="/pointBio">Points Biodiversités</ReactBootStrap.Nav.Link>
+                            <Link to="/">
+                                <ReactBootStrap.Nav.Link href="/">Acceuil</ReactBootStrap.Nav.Link>
+                            </Link>
+                            <Link to="/parcours">
+                                <ReactBootStrap.Nav.Link  href="/parcours">Parcours</ReactBootStrap.Nav.Link>
+                            </Link>
+                            <Link to="/pointBio">
+                                <ReactBootStrap.Nav.Link className="mr-auto" href="/pointBio">Points Biodiversités</ReactBootStrap.Nav.Link>
+                            </Link>
+
                          </ReactBootStrap.Nav>
                         <ReactBootStrap.Nav>
-                            <ReactBootStrap.Nav.Link href="/compte">Mon Compte</ReactBootStrap.Nav.Link>
+                            <Link to="/compte">
+                                <ReactBootStrap.Nav.Link   href="/compte">Mon Compte</ReactBootStrap.Nav.Link>
+                            </Link>
                         </ReactBootStrap.Nav>
                     </ReactBootStrap.Navbar.Collapse>
                 </ReactBootStrap.Navbar>
