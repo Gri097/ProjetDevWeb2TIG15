dda42a8d429011d20d100997071a4f97b4fa4643
Robin <robin@castermane.be>
2020-05-17 21:31:26 +0200

    us3 update

--- FrontEnd/biodiversite/src/components/PointsBiodiversites/PointBio.js
+++ FrontEnd/biodiversite/src/components/PointsBiodiversites/PointBio.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import {Link} from "react-router-dom";
 import logoVert from '../../images/logo-vert.png';
 import './PointBio.css';
@@ -6,43 +6,44 @@ import './PointBio.css';
 class PointBio extends Component {
 
     render() {
-        const parcours = this.props.parcours === 1 ?
-                            (<div className="science">
-                                <img src={logoVert} alt="Logo-vert" style={{width:'30px'}}/>
-                                <Link to={`/pointBio/${this.props.id}`} style={{textDecoration:'none',color:'black', fontSize:'18px', margin:'5px 5px 5px 5px'}}>
-                                    {this.props.nomScien}
-                                </Link>
-                            </div>) :
-                            (this.props.parcours === 2 ?
-                                (<div className="cyclotron">
-                                    <img src={logoVert} alt="Logo-vert" style={{width:'30px'}}/>
-                                    <Link to={`/pointBio/${this.props.id}`} style={{textDecoration:'none',color:'black', fontSize:'18px', margin:'5px 5px 5px 5px'}}>
-                                        {this.props.nomScien}
-                                    </Link>
-                                </div>) :
-                                (this.props.parcours === 3 ?
-                                    (<div className="lac">
-                                        <img src={logoVert} alt="Logo-vert" style={{width:'30px'}}/>
-                                        <Link to={`/pointBio/${this.props.id}`} style={{textDecoration:'none',color:'black', fontSize:'18px', margin:'5px 5px 5px 5px'}}>
-                                            {this.props.nomScien}
-                                        </Link>
-                                    </div>) :
-                                    (this.props.parcours === 4 ?
-                                        (<div className="jb">
-                                            <img src={logoVert} alt="Logo-vert" style={{width:'30px'}}/>
-                                            <Link to={`/pointBio/${this.props.id}`} style={{textDecoration:'none',color:'black', fontSize:'18px', margin:'5px 5px 5px 5px'}}>
-                                                {this.props.nomScien}
-                                            </Link>
-                                        </div>) :
-                                        (<div className="parcM">
-                                            <img src={logoVert} alt="Logo-vert" style={{width:'30px'}}/>
-                                            <Link to={`/pointBio/${this.props.id}`} style={{textDecoration:'none',color:'black', fontSize:'18px', margin:'5px 5px 5px 5px'}}>
-                                                {this.props.nomScien}
-                                            </Link>
-                                        </div>))))
-        return (
 
-            {parcours}
+        return (
+            <Fragment>
+                {this.props.parcours === 1 ?
+                (<div className="science">
+                <img src={logoVert} alt="Logo-vert" style={{width:'30px'}}/>
+                <Link to={`/pointBio/${this.props.id}`} style={{textDecoration:'none',color:'black', fontSize:'18px', margin:'5px 5px 5px 5px'}}>
+                    {this.props.nomScien}
+                </Link>
+            </div>) :
+                (this.props.parcours === 2 ?
+                (<div className="cyclotron">
+                <img src={logoVert} alt="Logo-vert" style={{width:'30px'}}/>
+                <Link to={`/pointBio/${this.props.id}`} style={{textDecoration:'none',color:'black', fontSize:'18px', margin:'5px 5px 5px 5px'}}>
+                    {this.props.nomScien}
+                </Link>
+            </div>) :
+                (this.props.parcours === 3 ?
+                (<div className="lac">
+                <img src={logoVert} alt="Logo-vert" style={{width:'30px'}}/>
+                <Link to={`/pointBio/${this.props.id}`} style={{textDecoration:'none',color:'black', fontSize:'18px', margin:'5px 5px 5px 5px'}}>
+                    {this.props.nomScien}
+                </Link>
+            </div>) :
+                (this.props.parcours === 4 ?
+                (<div className="jb">
+                <img src={logoVert} alt="Logo-vert" style={{width:'30px'}}/>
+                <Link to={`/pointBio/${this.props.id}`} style={{textDecoration:'none',color:'black', fontSize:'18px', margin:'5px 5px 5px 5px'}}>
+                    {this.props.nomScien}
+                </Link>
+            </div>) :
+                (<div className="parcM">
+                <img src={logoVert} alt="Logo-vert" style={{width:'30px'}}/>
+                <Link to={`/pointBio/${this.props.id}`} style={{textDecoration:'none',color:'black', fontSize:'18px', margin:'5px 5px 5px 5px'}}>
+                    {this.props.nomScien}
+                </Link>
+            </div>))))}
+            </Fragment>
         );
     }
 }
